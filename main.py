5648b3838b2fa58e77ef541f7a4111e85bc1af0f
Michael Catelli <MCATELLI@MITRE.ORG>
2025-06-12 15:05:06 -0400

    Finished basic implementation of third feature (Data breach checker)

--- main.py
+++ main.py
@@ -5,14 +5,16 @@ Personal Security Toolkit
 
 Fratures:
 
-1. Password strength checker
+1. Password strength checker (Finished basic implementation)
 2. Check if a file is malicious using virustotal
 3. Data breach checker - Haveibeenpwned API
 4. Network traffic analyzer
 5. Encrypted notepad
 
 '''
-import zipfile
+import requests
+import httpx
+import json
 
 def main():
     print("Welcome to the Personal Security Toolkit!\n")
@@ -37,6 +39,7 @@ def main():
     else:
         print("Invalid choice, please try again.")
 
+# Checks the characteristics of the inputted password and calls to password_search function
 def password_strength_checker():
     user_password = input("Enter a password to check its strength: ")
     recommendations = ["Password strength recommendations:"]
@@ -62,16 +65,35 @@ def password_strength_checker():
     print("\n".join(recommendations) + "\n")
     password_search("10k-most-common.txt", user_password)
      
-
+# Cross checks the user password against a file containing the 10k most commonly used passwords
 def password_search(file_path, user_password):
     with open(file_path, "r", encoding="utf-8") as f:
         content = f.read() 
         if user_password in content:
             print(f"'{user_password}' is one of the top 10k most commonly used passwords in the world. Change your password immediately!")
 
+def data_breach_checker():
+    email = input("Enter your email: ")
+    url = f"https://api.xposedornot.com/v1/check-email/{email}"    
+    with httpx.Client(verify=False) as client:
+        try:
+            response = client.get(url)
+            response.raise_for_status()  # Raise an error for bad responses
+        except httpx.HTTPStatusError as e:
+            print(f"Error: {e.response.status_code} - {e.response.text}")
+            return
+        except httpx.RequestError as e:
+            print(f"Request failed: {e}")
+            return
+    
+        data = response.json()
+        breaches = ", ".join(data["breaches"][0])
+        email = data["email"]
+        formatted_output = f"   Email: {email}\n   Breaches: {breaches}"
+        print(formatted_output)
 
 
-        
+# Constantly runs the main function until the user decides to exit
 while True:
     main()
     again = input("Would you like to use another tool? Yes[Y] / No[N]: ")
